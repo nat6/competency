8c1e31cfae6ba1dcf807d0962f422fbdd2f63645
n.antonova <n.antonova@dats.team>
2025-10-27 18:56:37 +0300

    chore: static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
+// next.config.ts
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'competency' // имя репо в GitHub
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default {
+  output: 'export',          // ключ к статическому билду
+  images: { unoptimized: true }, // для GitHub Pages (нет серверной оптимизации)
+  trailingSlash: true,       // /path/ -> /path/index.html (важно для Pages)
+  basePath: isProd ? `/${repo}` : '',     // т.к. это project pages: user.github.io/<repo>
+  assetPrefix: isProd ? `/${repo}/` : '', // чтобы подтягивались CSS/JS/изображения
+}
